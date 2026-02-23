b662c270fa5f86d599bbe11a4b9ad3defff93410
thisgleammm <thisgleam@gmail.com>
2026-02-23 14:13:31 +0700

    Delete

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -111,4 +111,16 @@ class CategoryTest extends TestCase
         self::assertEquals(10, $total);
 
     }
+
+    public function testDelete()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::find("FOOD");
+        $result = $category->delete();
+        self::assertTrue($result);
+
+        $total = Category::count();
+        self::assertEquals(0, $total);
+    }
 }
