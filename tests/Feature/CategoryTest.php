3015877e8c63f23e0261b3a1793ede5b6c53ac1b
thisgleammm <thisgleam@gmail.com>
2026-02-23 14:00:23 +0700

    Insert

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function testInsert(): void
+    {
+        $category = new Category();
+        $category->id = "GADGET";
+        $category->name = "Gadget";
+        $result = $category->save();
+
+        self::assertTrue($result);
+    }
+}
